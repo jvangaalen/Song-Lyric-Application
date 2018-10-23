85585b8acdd0ebfca00f781f471ea3d5ffc47ae0
Jerome vanGaalen <jerome.vangaalen@gmail.com>
2018-10-23 19:09:12 -0400

    Added a Lyrics class with Id, and Result properties.

---
+++ Song Lyric Application/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Song_Lyric_Application
+{
+    public class Song
+    {
+        // Properties that will store data from Song csv
+        public int Id { get; set; }
+        public string Artist { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class Lyrics
+    {
+        public int Id {get; set;}
+        public string Result { get; set; }
+    }
+}
